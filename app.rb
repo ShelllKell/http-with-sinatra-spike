7c86528a93beff3df8bfa77e99e7888115d0371c
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-26 16:27:24 -0600

    Cry

--- app.rb
+++ app.rb
@@ -1,17 +1,44 @@
 require "sinatra/base"
-require "sinatra/reloader"
+
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
+
 
   def initialize
     super
-    @items = []
+    @students = {:student_1 => "Brad", :student_2 => "Layne", :student_3 => "Miji"}
   end
 
+
   get "/" do
-    "items length: #{@items.length}"
+    "items length: #{@students.length}" +
+    "<br>" +
+    "<a href='/students'>Go to students page</a>"
+
   end
+
+
+  get "/students/?" do
+    filter = params[:sort]
+    filtered_students = @students
+    unless filter == nil
+     filtered_students = @students.select { |key, value| value.include?(filter) }
+    end
+
+  erb :students, :locals => {:students => filtered_students}
+
+  end
+
+
+  get "/students/:name" do
+    name = params[:name]
+    erb :student_page, :locals => {:name => name}
+  end
+
+
+
+
+
 
   run! if app_file == $0
 end
